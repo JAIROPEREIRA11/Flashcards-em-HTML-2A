e6df2a84bb58bf40da1ab3d47f2c99c96bbdb7db
JAIROPEREIRA11 <105813968+JAIROPEREIRA11@users.noreply.github.com>
2025-11-12 11:08:22 -0300

    .

--- app.js
+++ app.js
@@ -2,8 +2,8 @@ function criaCartao(categoria, pergunta, resposta) {
     let container = document.getElementById('container')
     let cartao = document.createElement('article')
     cartao.className = 'cartao'
-    
-     cartao.innerHTML = `
+
+    cartao.innerHTML = `
     <div class="cartao__conteudo">
     <h3>${categoria}</h3>
     <div class="cartao__conteudo__pergunta">
@@ -13,6 +13,7 @@ function criaCartao(categoria, pergunta, resposta) {
         <p>${resposta}</p>
     </div>
     </div>
+    `
 
     let respostaEstaVisivel = false
 
